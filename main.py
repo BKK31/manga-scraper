ea5ea2c239683015b0cec332cbda1b7577eac33e
Bhargava K K <bhargavakk13@gmail.com>
2025-04-09 11:14:51 +0530

    Getting manga title and thumbnail

--- main.py
+++ main.py
@@ -15,3 +15,16 @@ soup = BeautifulSoup(response.text, "html.parser")
 
 print("Page Title:", soup.title.text)
 
+
+manga_cards = soup.find_all("div", class_="item item-spc")
+
+for card in manga_cards:
+    print("")
+    title_tag = card.find("h3", class_="manga-name").find("a")
+    title = title_tag.text.strip()
+
+    relative_url = title_tag["href"]
+    full_url = "https://mangareader.to" + relative_url
+
+    thumbnail_tag = card.find("a", class_="manga-poster").find("img")
+    thumbnail_url = thumbnail_tag["src"]
